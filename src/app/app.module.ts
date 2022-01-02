d327f3924c40ee54d01560461cd05460c9016d53
bankole <bankoleibrahim96@gmail.com>
2022-01-02 23:47:23 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product/product-detail.guard';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent,
+  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+    {path: 'product', component: ProductListComponent},
+    {path: 'products/:id', canActivate:[ProductDetailGuard], component: ProductDetailComponent},
+    {path: 'welcome', component: WelcomeComponent},
+    {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+    {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
+  ])],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
